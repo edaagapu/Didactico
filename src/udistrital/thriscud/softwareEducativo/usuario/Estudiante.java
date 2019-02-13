a9157144e1a42c19508b961c67c60771c9b9c6b6
anferente20 <34009575+anferente20@users.noreply.github.com>
2019-02-12 21:00:07 -0500

    Clases profesor y Estudiate (sin BD, ni Test)

---
+++ src/udistrital/thriscud/softwareEducativo/usuario/Estudiante.java
@@ -0,0 +1,13 @@
+package udistrital.thriscud.softwareEducativo.usuario;
+/**
+ * fecha: 12.2.19
+ * @author anfer
+ *
+ */
+public class Estudiante extends Usuario{
+
+	public Estudiante(String nombre, String apellido, String nickName, String contrasena) {
+		super(nombre, apellido, nickName, contrasena);
+	}
+
+}
---
+++ src/udistrital/thriscud/softwareEducativo/usuario/Profesor.java
@@ -0,0 +1,14 @@
+package udistrital.thriscud.softwareEducativo.usuario;
+/**
+ * fecha: 12.2.19
+ * @author anfer
+ *
+ */
+public class Profesor extends Usuario{
+
+	public Profesor(String nombre, String apellido, String nickName, String contrasena) {
+		super(nombre, apellido, nickName, contrasena);
+	}
+
+	
+}
